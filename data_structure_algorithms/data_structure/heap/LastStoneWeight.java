4f20095f6ace39fe19baac58c9268ddb16f24b7c
Vikas Yadav <do.vikas1369@gmail.com>
2024-04-02 17:09:05 +0100

    Added priority queue problem for last stone weight

---
+++ data_structure_algorithms/data_structure/heap/LastStoneWeight.java
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/last-stone-weight/
+*/
+
+class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        if(stones.length == 1){
+            return stones[0];
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        pq.addAll(Arrays.stream(stones).boxed().collect(Collectors.toList()));
+
+        while(pq.size()>1){
+            int mostHeavy = pq.poll();
+            int secondMostHeavy = pq.poll();
+            if(mostHeavy == secondMostHeavy){
+                //do nothing
+            }else if(mostHeavy > secondMostHeavy){
+                pq.offer(mostHeavy-secondMostHeavy);
+            }
+        }
+        return pq.size()==1 ? pq.poll(): 0;
+    }
+}

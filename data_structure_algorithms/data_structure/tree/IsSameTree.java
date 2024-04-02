c9092d04f25f27cb27b4a3b99d6cec08dfb14fd7
Vikas Yadav <do.vikas1369@gmail.com>
2024-04-02 14:11:16 +0100

    Added some tree based problems

---
+++ data_structure_algorithms/data_structure/tree/IsSameTree.java
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/same-tree/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q== null){
+            return true;
+        }else if(p == null && q!= null){
+            return false;
+        }else if(q == null && p!= null){
+            return false;
+        }else if(p.val != q.val){
+            return false;
+        }else{
+            boolean leftFlag = isSameTree(p.left, q.left);
+            boolean rightFlag = isSameTree(p.right, q.right);
+            return leftFlag && rightFlag;
+        }
+    }
+}
